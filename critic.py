212cf4c4b96c17a9c9fd543819733d3255261d6e
Alan Peterson <apeterson056@febe.pads.ufrj.br>
2024-01-25 16:32:41 -0300

    File for critic generator

---
+++ critic.py
@@ -0,0 +1,101 @@
+from os import environ
+
+environ["CUDA_VISIBLE_DEVICES"]="1"
+
+from tensorflow.keras import layers
+from tensorflow import keras
+from tensorflow.keras.utils import plot_model
+
+
+model_name = "D-Teste1"
+
+def conv_block(
+    x,
+    filters,
+    activation,
+    kernel_size=(3, 3),
+    strides=(1, 1),
+    padding="same",
+    use_bias=True,
+    use_bn=False,
+    use_dropout=False,
+    drop_value=0.5,
+):
+    x = layers.Conv2D(
+        filters, kernel_size, strides=strides, padding=padding, use_bias=use_bias
+    )(x)
+    if use_bn:
+        x = layers.BatchNormalization()(x)
+    x = activation(x)
+    if use_dropout:
+        x = layers.Dropout(drop_value)(x)
+    return x
+
+def get_discriminator_model():
+    img_input = layers.Input(shape=(64,64,1))
+    x = layers.ZeroPadding2D((2, 2))(img_input)
+    x = conv_block(
+        x,
+        64,
+        kernel_size=(5, 5),
+        strides=(2, 2),
+        use_bn=False,
+        use_bias=True,
+        activation=layers.LeakyReLU(0.2),
+        use_dropout=False,
+        drop_value=0.3,
+    )
+    x = conv_block(
+        x,
+        128,
+        kernel_size=(5, 5),
+        strides=(2, 2),
+        use_bn=False,
+        activation=layers.LeakyReLU(0.2),
+        use_bias=True,
+        use_dropout=True,
+        drop_value=0.3,
+    )
+    x = conv_block(
+        x,
+        256,
+        kernel_size=(5, 5),
+        strides=(2, 2),
+        use_bn=False,
+        activation=layers.LeakyReLU(0.2),
+        use_bias=True,
+        use_dropout=True,
+        drop_value=0.3,
+    )
+    x = conv_block(
+        x,
+        512,
+        kernel_size=(5, 5),
+        strides=(2, 2),
+        use_bn=False,
+        activation=layers.LeakyReLU(0.2),
+        use_bias=True,
+        use_dropout=False,
+        drop_value=0.3,
+    )
+
+    x = layers.Flatten()(x)
+    x = layers.Dropout(0.2)(x)
+    x = layers.Dense(1)(x)
+
+    d_model = keras.models.Model(img_input, x, name="discriminator")
+    return d_model
+
+
+model = get_discriminator_model()
+
+
+model_json = model.to_json(indent = 4)
+
+with open("Models/Discriminators/" + model_name + ".json", "w") as json_file:
+    json_file.write(model_json)
+    json_file.close()
+
+plot_model(model=model, to_file="Models/Discriminators/Diagram/" + model_name + '.png', show_shapes=True, rankdir= "TB", expand_nested=True )
+
+print(model.count_params())
